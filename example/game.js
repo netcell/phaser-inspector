3c101a33f22fac1ed39eb0a6873677366ebfd1c8
Nguyen Tuan Anh <anhnt.fami@gmail.com>
2015-09-10 14:57:27 +0700

    update example

--- example/game.js
+++ example/game.js
@@ -1,3 +1,10 @@
+function MyClass(game){
+  Phaser.Sprite.apply(this, arguments);
+  game.add.existing(this);
+}
+MyClass.prototype = Object.create(Phaser.Sprite.prototype);
+MyClass.prototype.constructor = MyClass;
+
 var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'game', {
 	preload: function preload() {
 		game.load.atlasXML('blueSheet', 'UIpack/blueSheet.png', 'UIpack/blueSheet.xml');
@@ -8,6 +15,14 @@ var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'game', {
 		this.image.anchor.set(0.5, 0.5);
 		this.textureRender = game.add.renderTexture(game.width, game.height);
 		this.textureRenderedSprite = game.add.sprite(0, 0, this.textureRender);
+
+		this.myGroup = game.add.group();
+		this.mySprite = new MyClass(game, 250, 150, 'blueSheet', 'blue_button02.png');
+		this.mySprite.anchor.set(0.5, 0.5);
+		this.myGroup.add(this.mySprite)
+		this.mySprite2 = new MyClass(game, 350, 250, 'blueSheet', 'blue_button02.png');
+		this.mySprite2.anchor.set(0.5, 0.5);
+		this.myGroup.add(this.mySprite2)
 	},
 	update: function update() {
 		var pointer = game.input.activePointer;
