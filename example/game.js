ee82fade226c1e85f9fb3b74941afe63ccbfa24f
Nguyen Tuan Anh <anhnt.fami@gmail.com>
2015-09-14 15:30:52 +0700

    Remove renderTexture example for clear

--- example/game.js
+++ example/game.js
@@ -11,10 +11,6 @@ var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'game', {
 	},
 	create: function create() {
 		game.plugins.add(Phaser.Plugin.Inspector);
-		this.image = game.make.sprite(0, 0, 'blueSheet', 'blue_button02.png');
-		this.image.anchor.set(0.5, 0.5);
-		this.textureRender = game.add.renderTexture(game.width, game.height);
-		this.textureRenderedSprite = game.add.sprite(0, 0, this.textureRender);
 
 		this.myGroup = game.add.group();
 		this.mySprite = new MyClass(game, 250, 150, 'blueSheet', 'blue_button02.png');
@@ -23,11 +19,5 @@ var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'game', {
 		this.mySprite2 = new MyClass(game, 350, 250, 'blueSheet', 'blue_button02.png');
 		this.mySprite2.anchor.set(0.5, 0.5);
 		this.myGroup.add(this.mySprite2)
-	},
-	update: function update() {
-		var pointer = game.input.activePointer;
-		if (!pointer.position.isZero()) {
-			this.textureRender.renderXY(this.image, pointer.x, pointer.y, true);
-		}
 	}
 })
